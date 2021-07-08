8573167592310a520e2dce318f581ac90867801b
Luis Miranda <puzzledcrib74@outlook.com>
2021-07-08 06:34:32 +0000

    New Commit for Building

---
+++ themes/deck_blog/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.deck_blog = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
