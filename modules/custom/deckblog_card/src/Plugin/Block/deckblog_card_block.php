bbac335625f208e33d662752317b3a2357435f9d
yohel715 <yohel715@gmail.com>
2021-07-28 19:19:38 -0600

    custom block

---
+++ modules/custom/deckblog_card/src/Plugin/Block/deckblog_card_block.php
@@ -0,0 +1,51 @@
+<?php
+
+//ruta de la clase
+namespace Drupal\drupal_block\Plugin\Block;
+
+//importa la clase que usa drupal para generar los bloques
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * @Block(
+ *   id = "Drupal Block",
+ *   admin_label = @translation("Drupal Block"),
+ *   category = @translation("Drupal Block"),
+ * )
+ */
+
+
+class CardBlock extends BlockBase{
+
+    /**
+     * {@inheritdoc }
+     */
+    
+    
+    /**
+    * Metodo que genera el bloque y 
+    * devuelve el contenido a renderizar 
+    */
+    public function build()
+    {
+        return [
+            '#markup' => $this->getCard(), //función que trae las cartas
+            '#cache' => [
+                'max-age' => 0,
+            ]
+        ];
+    }
+    private function getCard()
+    {
+        /**
+         * variable
+         * $card
+         */
+        $card = [
+            'Hola, que tal',
+            'Otras vez por aquí?',
+            'Nos vemos pronto!!!'
+        ];
+        return $card[array_rand($card)]; //se devuelve mediante un array_rand(RANDOM)
+    }
+}
