36cbc2f00ca5399a716bdbd3a51a4a409a4ec983
yohel715 <yohel715@gmail.com>
2021-07-28 19:29:41 -0600

    deckblock

---
+++ modules/custom/deckblock/src/Plugin/Block/DeckBlock.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace  Drupal\deckblock\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * @Block(
+ *   id = "Deck Block",
+ *   admin_label = @translation("Deck Block"),
+ *   category = @translation("Deck Block"),
+ * )
+ */
+
+
+class DeckBlock extends BlockBase{
+
+  /**
+   * {@inheritdoc }
+   */
+
+  public function build(){
+    return [
+      '#markup' => $this->getFrases(),
+      '#cache' => [
+        'max-age' => 0,
+      ]
+    ];
+  }
+   private function getFrases(){
+    $frase = [
+      'Hola, que tal',
+      'Otras vez por aquí?',
+      'Nos vemos pronto!!!'
+    ];
+    return $frase[array_rand($frase)];
+   }
+}
